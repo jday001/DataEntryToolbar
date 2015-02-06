d5ce82335bf6e0ad9021259120210106eb6d2d7c
jeff <jday@jdayapps.com>
2015-02-06 17:49:02 -0500

    Initial Commit

---
+++ DataEntryToolbar/DataEntryToolbar.h
@@ -0,0 +1,19 @@
+//
+//  DataEntryToolbar.h
+//  DataEntryToolbar
+//
+//  Created by Jeff Day on 2/6/15.
+//  Copyright (c) 2015 JDay Apps, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataEntryToolbar.
+FOUNDATION_EXPORT double DataEntryToolbarVersionNumber;
+
+//! Project version string for DataEntryToolbar.
+FOUNDATION_EXPORT const unsigned char DataEntryToolbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataEntryToolbar/PublicHeader.h>
+
+
